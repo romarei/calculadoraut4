f7eb1eae44739368d5bf9ec2f1e9c8256b5443f3
Romarei Vega <romareiv@gmail.com>
2021-02-25 17:32:22 +0000

    paso 1 y 2

---
+++ src/main/java/calculadora.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author romar
+ */
+public class calculadora {
+    private int operador1;
+    private int operador2;
+    private int operacion;
+}
