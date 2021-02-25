6f0681d3414c40c93ac41539fcf7ab889f78517d
Romarei Vega <romareiv@gmail.com>
2021-02-25 17:35:36 +0000

    paso 3 y 4

--- src/main/java/calculadora.java
+++ src/main/java/calculadora.java
@@ -12,4 +12,46 @@ public class calculadora {
     private int operador1;
     private int operador2;
     private int operacion;
+
+    /**
+     * @return the operador1
+     */
+    public int getOperador1() {
+        return operador1;
+    }
+
+    /**
+     * @param operador1 the operador1 to set
+     */
+    public void setOperador1(int operador1) {
+        this.operador1 = operador1;
+    }
+
+    /**
+     * @return the operador2
+     */
+    public int getOperador2() {
+        return operador2;
+    }
+
+    /**
+     * @param operador2 the operador2 to set
+     */
+    public void setOperador2(int operador2) {
+        this.operador2 = operador2;
+    }
+
+    /**
+     * @return the operacion
+     */
+    public int getOperacion() {
+        return operacion;
+    }
+
+    /**
+     * @param operacion the operacion to set
+     */
+    public void setOperacion(int operacion) {
+        this.operacion = operacion;
+    }
 }
